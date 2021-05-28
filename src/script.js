7d08ce1f39734b014055dab4517391b42ee3f8af
Shehryar <shehryar.dev@gmail.com>
2021-05-28 15:53:21 +0400

    gsap scroll

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import gsap from 'gsap';
 
 // Debug
 const gui = new dat.GUI();
@@ -12,9 +13,7 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Sizes
- */
+//Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -34,18 +33,14 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Camera
- */
+// Camera
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.x = 0;
@@ -53,43 +48,81 @@ camera.position.y = 0;
 camera.position.z = 2;
 scene.add(camera);
 
+gui.add(camera.position, 'y', -10, 0);
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+// Texture loader
+const textureLoader = new THREE.TextureLoader();
 
 // Objects
-const geometry = new THREE.TorusGeometry(.7, .2, 16, 100);
+const geometry = new THREE.PlaneBufferGeometry(1, 1.3)
+for (let i = 0; i < 5; i++) {
+    const material = new THREE.MeshBasicMaterial({
+        map: textureLoader.load(`/imgs/${i + 1}.jpg`)
+    });
 
-// Materials
+    const img = new THREE.Mesh(geometry, material);
+    img.position.set(1, i * -1.8);
+    scene.add(img);
+}
 
-const material = new THREE.MeshBasicMaterial();
-material.color = new THREE.Color(0xff0000);
-
-// Mesh
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+let objects = []
+scene.traverse((object) => {
+    if (object.isMesh) objects.push(object);
+});
 
 // Lights
-
 const pointLight = new THREE.PointLight(0xffffff, 0.1);
 pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+// Mouse
+let y = 0;
+let position = 0;
+window.addEventListener('wheel', (event) => {
+    y = event.deltaY * 0.0007;
+});
 
-/**
- * Animate
- */
+const mouse = new THREE.Vector2();
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1;
+    mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+})
+
+//Animate
+const rayCaster = new THREE.Raycaster();
 
 const clock = new THREE.Clock();
-
 const tick = () => {
 
     const elapsedTime = clock.getElapsedTime();
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime;
+    position += y
+    camera.position.y = -position;
+    y *= 0.95;
+
+    // Ray caster
+    rayCaster.setFromCamera(mouse, camera);
+    const intersects = rayCaster.intersectObjects(objects);
+    for (const intersect of intersects) {
+        gsap.to(intersect.object.scale, { x: 1.7, y: 1.7 });
+        gsap.to(intersect.object.rotation, { y: -0.5 });
+        gsap.to(intersect.object.position, { z: -0.9 });
+    }
+
+    for (const object of objects) {
+        if (!intersects.find(intersect => intersect.object === object)) {
+            gsap.to(object.scale, { x: 1, y: 1 });
+            gsap.to(object.rotation, { y: 0 });
+            gsap.to(object.position, { z: 0 });
+        }
+    }
 
     // Update Orbital Controls
     // controls.update()
