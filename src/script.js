d710ac4d3a64e6e8912968a0fdcccb314435c9cc
Shehryar <shehryar.dev@gmail.com>
2021-07-21 01:18:52 +0400

    animated blender phone

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import gsap from 'gsap';
 
 // Debug
 const gui = new dat.GUI();
@@ -38,7 +40,8 @@ window.addEventListener('resize', () => {
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -50,25 +53,39 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 2;
+camera.position.z = 1;
 scene.add(camera);
 
 
+// Loaders
+const gltfLoader = new GLTFLoader();
+
+// Timeline
+let tl = gsap.timeline();
+
 // Objects
-const geometry = new THREE.TorusGeometry(.7, .2, 16, 100);
+gltfLoader.load('phone.gltf', (gltf) => {
+    gltf.scene.scale.set(0.3, 0.3, 0.3);
+    gltf.scene.rotation.set(0, 3.3, 0);
+    scene.add(gltf.scene);
+
+    gui.add(gltf.scene.rotation, 'x').min(0).max(9)
+    gui.add(gltf.scene.rotation, 'y').min(0).max(9)
+    gui.add(gltf.scene.rotation, 'z').min(0).max(9)
+
+    tl.to(gltf.scene.rotation, { y: 4.7, duration: 1 });
+    tl.to(gltf.scene.scale, { x: 0.2, y: 0.2, z: 0.2, duration: 1 }, "-=1");
+    tl.to(gltf.scene.position, { x: .5 });
+    tl.to(gltf.scene.rotation, { y: 4.1, duration: 1 }, "-=0.5");
+});
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial();
-material.color = new THREE.Color(0xff0000);
-
 // Mesh
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
+const pointLight = new THREE.AmbientLight(0xffffff, 1);
 pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
@@ -89,7 +106,7 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime();
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime;
+    // sphere.rotation.y = .5 * elapsedTime;
 
     // Update Orbital Controls
     // controls.update()
